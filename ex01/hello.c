8e8d2612b9d86e376f3cf34c54c82911cf411113
lhy628629 <3110869140@qq.com>
2026-06-04 17:42:00 +0800

    v2:添加scanf键盘输入功能

--- ex01/hello.c
+++ ex01/hello.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 int main(void){
-    printf("Hello World!");
+    char buf[128];
+    printf("Hello World!\n");
+    printf("ÇëÊäÈëÄÚÈİ£º");
+    scanf("%s",buf);
+    printf("Êä³ö£º%s\n",buf);
     return 0;
 }
